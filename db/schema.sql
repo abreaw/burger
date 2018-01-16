0fce8aff5dc37027a7ca639168efdd2c8ffb28f3
Brea Torres <abreaw328@gmail.com>
2018-01-15 19:31:02 -0500

    initial DB setup with server.js dependencies and package.json files added

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+-- * Create the `burgers_db`.
+CREATE DATABASE burgers_db;
+
+-- * Switch to or use the `burgers_db`.
+USE burgers_db;
+
+-- * Create a `burgers` table with these fields:
+	--   * **id**: an auto incrementing int that serves as the primary key.
+	--   * **burger_name**: a string.
+	--   * **devoured**: a boolean.
+	--   * **date**: a TIMESTAMP.
+
+CREATE TABLE IF NOT EXISTS `burgers` 
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(255) NOT NULL,
+	`devoured` VARCHAR(255) NOT NULL DEFAULT false,
+	`date` DATETIME NOT NULL,
+
+	PRIMARY KEY (`id`)
+);
+
+SELECT * FROM burgers ORDER BY date DESC;
+
+
